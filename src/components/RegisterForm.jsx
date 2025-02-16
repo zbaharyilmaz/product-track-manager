3ec161f20c702e558831bab25214b0f296fdd020
zeynepyilmaz <ylmzbhr1@gmail.com>
2025-02-16 22:21:41 +0300

    Implement registration form with validation and remove unused assets

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,93 @@
+import { TextField } from "@mui/material";
+
+const RegisterForm = () => {
+    ({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      })=>
+  return (
+    <div>
+         <form onSubmit={handleSubmit}> {/* Fixed: Add onSubmit to form */}
+                <TextField
+                  name="username"
+                  value={values.username}
+                  label="Username"
+                  variant="outlined"
+                  fullWidth
+                  onChange={handleChange}
+                  error={touched.username && Boolean(errors.username)}
+                  helperText={touched.username && errors.username}
+                  onBlur={handleBlur}
+                  margin="dense"
+                />
+                <TextField
+                  name="firstName"
+                  value={values.firstName}
+                  label="First Name"
+                  variant="outlined"
+                  fullWidth
+                  onChange={handleChange}
+                  error={touched.firstName && Boolean(errors.firstName)}
+                  helperText={touched.firstName && errors.firstName}
+                  onBlur={handleBlur}
+                  margin="dense"
+                />
+                <TextField
+                  name="lastName"
+                  value={values.lastName}
+                  label="Last Name"
+                  variant="outlined"
+                  fullWidth
+                  onChange={handleChange}
+                  error={touched.lastName && Boolean(errors.lastName)}
+                  helperText={touched.lastName && errors.lastName}
+                  onBlur={handleBlur}
+                  margin="dense"
+                />
+                <TextField
+                  name="email"
+                  value={values.email}
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  onChange={handleChange}
+                  error={touched.email && Boolean(errors.email)}
+                  helperText={touched.email && errors.email}
+                  onBlur={handleBlur}
+                  margin="dense"
+                  type="email"
+                />
+                <TextField
+                  name="password"
+                  value={values.password}
+                  label="Password"
+                  variant="outlined"
+                  fullWidth
+                  onChange={handleChange}
+                  error={touched.password && Boolean(errors.password)}
+                  helperText={touched.password && errors.password}
+                  onBlur={handleBlur}
+                  margin="dense"
+                  type="password"
+                />
+                <Button
+                  fullWidth
+                  variant="contained"
+                  type="submit"
+                  color="primary"
+                  margin="dense"
+                  disabled={isSubmitting}
+                >
+                  SEND
+                </Button>
+              </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,15 +2,46 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 import LockIcon from "@mui/icons-material/Lock";
-import image from "../assets/regi.avif";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
-import { Box, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import AuthHeader from "../components/AuthHeader";
 import AuthImage from "../components/AuthImage";
 import { Formik } from "formik";
+import * as Yup from "yup";
+import register from "../assets/register.png";
+import RegisterForm from "../components/RegisterForm";
 
 const Register = () => {
+  const signupSchema = Yup.object().shape({
+    username: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    firstName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    lastName: Yup.string()
+      .min(2, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    email: Yup.string().email("Invalid email").required("Required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 characters")
+      .max(15, "Too Long!")
+      .required("Required")
+      .matches(/[a-z]/, "Password must contain lowercase letters")
+      .matches(/[A-Z]/, "Password must contain uppercase letters")
+      .matches(/[0-9]/, "Password must contain numbers")
+      .matches(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, "Password must contain special characters")
+     
+  });
+
+  const handleSubmit = (values) => {
+    console.log(values); 
+  };
+
   return (
     <Container maxWidth="lg">
       <Grid
@@ -48,42 +79,26 @@ const Register = () => {
           <Formik
             initialValues={{
               username: "",
-              firstname: "",
+              firstName: "",
+              lastName: "",
               email: "",
               password: "",
             }}
-            validate={{}}
-            onSubmit={{}}
+            validationSchema={signupSchema} 
+            onSubmit={(values)=>console.log(values)}
           >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-              isSubmitting,
-            }) => (
-              <form action="">
-                <TextField
-                  name="username"
-                  value={values.username}
-                  // id="outlined-basic"
-                  label="Username"
-                  variant="outlined"
-                  fullWidth
-                  onChange={handleChange}
-                />
-              </form>
-            )}
+            component={(props)=>(<RegisterForm {...props}/>)}
+         
           </Formik>
 
           <Box sx={{ textAlign: "center", mt: 2, color: "secondary.main" }}>
             <Link to="/">Already have an account? Sign in</Link>
           </Box>
         </Grid>
-
-        <AuthImage image={image} />
+      <Box sx={{ width: "50%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <AuthImage image={register}  /> 
+      </Box>
+       
       </Grid>
     </Container>
   );
